a3743a71c0ff76cb4c42f0e01e290952292594e9
Ellington Kirby <ellingtonkirby31@gmail.com>
2024-06-27 17:24:47 +0000

    flash attention model

---
+++ models/dit3d_flash_attn.py
@@ -0,0 +1,460 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# GLIDE: https://github.com/openai/glide-text2im
+# MAE: https://github.com/facebookresearch/mae/blob/main/models_mae.py
+# --------------------------------------------------------
+
+import torch
+import torch.nn as nn
+import math
+from timm.models.vision_transformer import Mlp, Attention
+import numpy as np
+
+
+def modulate(x, shift, scale):
+    return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
+
+
+#################################################################################
+#               Embedding Layers for Timesteps and Class Labels                 #
+#################################################################################
+ 
+class PointEmbed(nn.Module):
+    """Point Embedding (keep the original argument list for compatibility)
+    """
+    def __init__(self, in_chans=3, embed_dim=768, num_neighbors=16):
+        super().__init__()
+
+        self.num_neighbors = num_neighbors
+
+        # # Normalize inputs
+        self.norm = nn.BatchNorm1d(in_chans)
+
+        # Point Embedding
+        self.conv1 = nn.Conv1d(in_chans, embed_dim, 1)
+
+        # Neighborhood embedding
+        self.conv2 = nn.Sequential(
+            nn.BatchNorm2d(in_chans),
+            nn.Conv2d(in_chans, embed_dim, 1, bias=False),
+            nn.BatchNorm2d(embed_dim),
+            # nn.ReLU(inplace=True),
+            nn.GELU(),
+            nn.Conv2d(embed_dim, embed_dim, 1, bias=False),
+        )
+
+        # Merge point and neighborhood embeddings
+        self.final = nn.Conv1d(2 * embed_dim, embed_dim, 1, bias=True, padding=0)
+
+    def forward(self, x):
+        # Point embedding
+        B, Cin, N = x.shape
+
+        # # Normalize input
+        x = self.norm(x)
+
+        # Point embedding
+        point_emb = self.conv1(x)
+
+        # compute nearest neighbors in 3D euclidean space
+        dist = -torch.norm(x.unsqueeze(-1) - x.unsqueeze(-2), dim=1, p=None)
+        knn = dist.topk(self.num_neighbors, largest=False) # Not efficient
+        neighbors = knn.indices.transpose(1,2) # [B K N]
+
+        gather = []
+        for ind_nn in range(1, neighbors.shape[1]):  # Remove first neighbors which is the center point
+            temp = neighbors[:, ind_nn : ind_nn + 1, :].expand(-1, x.shape[1], -1) # B Cin N
+            gather.append(torch.gather(x, 2, temp).unsqueeze(-1))
+        neigh_emb = torch.cat(gather, -1) - x.unsqueeze(-1)  # Size: (B x C x N) x K
+
+        # Embedding
+        neigh_emb = self.conv2(neigh_emb).max(-1)[0]
+
+        # Merge both embeddings
+        return self.final(torch.cat((point_emb, neigh_emb), dim=1))
+
+
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations.
+    """
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+
+class LabelEmbedder(nn.Module):
+    """
+    Embeds class labels into vector representations. Also handles label dropout for classifier-free guidance.
+    """
+    def __init__(self, num_classes, hidden_size, dropout_prob):
+        super().__init__()
+        use_cfg_embedding = dropout_prob > 0
+        self.embedding_table = nn.Embedding(num_classes + use_cfg_embedding, hidden_size)
+        self.num_classes = num_classes
+        self.dropout_prob = dropout_prob
+
+    def token_drop(self, labels, force_drop_ids=None):
+        """
+        Drops labels to enable classifier-free guidance.
+        """
+        if force_drop_ids is None:
+            drop_ids = torch.rand(labels.shape[0], device=labels.device) < self.dropout_prob
+        else:
+            drop_ids = force_drop_ids == 1
+        # print('token drop drop_ids:', drop_ids, drop_ids.shape)
+        labels = torch.where(drop_ids, self.num_classes, labels)
+        return labels
+
+    def forward(self, labels, train, force_drop_ids=None):
+        # use_dropout = self.dropout_prob > 0
+        # if (train and use_dropout) or (force_drop_ids is not None):
+        #     labels = self.token_drop(labels, force_drop_ids)
+        # print('token drop labels:', labels, labels.shape)
+        embeddings = self.embedding_table(labels)
+        return embeddings
+
+class ConditionEmbedder(nn.Module):
+    """
+    Embeds conditions into fourier features.
+    """
+    def __init__(self, hidden_size, frequency_embedding_size=256, num_cyclic_conditions=1):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+        self.num_cyclic_conditions = num_cyclic_conditions
+
+    @staticmethod
+    def cyclic_embedding(condition, dim):
+        batch_size, _ = condition.shape
+        half_dim = dim // 2
+
+        frequencies = (- torch.arange(0, half_dim) * np.log(10000) / (half_dim - 1)).exp()
+        frequencies = frequencies[None, None, :].repeat(batch_size, 1, 1).cuda()
+
+        sin_sin_emb = ((condition[:, :, None]).sin() * frequencies).sin()
+        sin_cos_emb = ((condition[:, :, None]).cos() * frequencies).sin()
+        emb = torch.cat([sin_sin_emb, sin_cos_emb], dim=2)
+        
+        if dim % 2:  # zero pad
+            emb = nn.functional.pad(emb, (0, 1), "constant", 0)
+        return emb
+    
+    @staticmethod
+    def positional_embedding(condition, dim):
+        half_dim = dim // 2
+        emb = np.ones(condition.shape[0]) * np.log(10000) / (half_dim - 1)
+        emb = torch.from_numpy(np.exp(np.arange(0, half_dim) * -emb[:,None])).float().to(torch.device('cuda'))
+        emb = condition[:, :, None] * emb[:, None, :]
+        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=2)
+        if dim % 2:  # zero pad
+            emb = nn.functional.pad(emb, (0, 1), "constant", 0)
+        return emb
+
+    def forward(self, c):
+        c_freq_cyc = self.cyclic_embedding(c[:, :self.num_cyclic_conditions], self.frequency_embedding_size)
+        c_freq_pos = self.positional_embedding(c[:, self.num_cyclic_conditions:], self.frequency_embedding_size)
+        c_freq = torch.concat((c_freq_cyc, c_freq_pos), 1)
+        c_emb = self.mlp(c_freq)
+        return c_emb
+    
+#################################################################################
+#                                 Core DiT Model                                #
+#################################################################################
+
+class DiTBlock(nn.Module):
+    """
+    A DiT block with adaptive layer norm zero (adaLN-Zero) conditioning.
+    """
+    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0, **block_kwargs):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.attn = Attention(hidden_size, num_heads=num_heads, qkv_bias=True, **block_kwargs)
+        
+        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        
+        mlp_hidden_dim = int(hidden_size * mlp_ratio)
+        approx_gelu = lambda: nn.GELU(approximate="tanh")
+
+        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
+        self.num_conditions = 8
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size * self.num_conditions, 6 * hidden_size, bias=True)
+        )
+
+
+    def forward(self, x, c):
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
+        x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa))
+        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))
+        return x
+
+
+class FinalLayer(nn.Module):
+    """
+    The final layer of DiT.
+    """
+    def __init__(self, hidden_size, out_channels):
+        super().__init__()
+        self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(hidden_size, out_channels, bias=True)
+        self.num_conditions = 8
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size * self.num_conditions, 2 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, c):
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        x = modulate(self.norm_final(x), shift, scale)
+        x = self.linear(x)
+        return x
+
+
+class DiT(nn.Module):
+    """
+    Diffusion model with a Transformer backbone.
+    """
+    def __init__(
+        self,
+        input_size=32,
+        patch_size=4,
+        in_channels=3,
+        hidden_size=1152,
+        depth=28,
+        num_heads=16,
+        mlp_ratio=4.0,
+        class_dropout_prob=0.1,
+        num_classes=1,
+        learn_sigma=False
+    ):
+        super().__init__()
+        self.learn_sigma = learn_sigma
+        self.in_channels = in_channels
+        self.out_channels = in_channels * 2 if learn_sigma else in_channels
+        self.num_heads = num_heads
+
+        self.x_embedder = PointEmbed(in_channels, hidden_size)
+        
+        self.t_embedder = TimestepEmbedder(hidden_size)
+        self.y_embedder = LabelEmbedder(num_classes, hidden_size, class_dropout_prob)
+        self.c_embedder = ConditionEmbedder(hidden_size)
+
+        self.blocks = nn.ModuleList([
+            DiTBlock(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)
+        ])
+        self.final_layer = FinalLayer(hidden_size, self.out_channels)
+
+        print("TODO init weights")
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Initialize transformer layers:
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+
+        # Initialize label embedding table:
+        nn.init.normal_(self.y_embedder.embedding_table.weight, std=0.02)
+
+        # Initialize timestep embedding MLP:
+        nn.init.normal_(self.t_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.t_embedder.mlp[2].weight, std=0.02)
+
+        # Initialize condition embedding MLP:
+        nn.init.normal_(self.c_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.c_embedder.mlp[2].weight, std=0.02)
+
+        # Zero-out adaLN modulation layers in DiT blocks:
+        for block in self.blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+
+        # Zero-out output layers:
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+
+    def forward(self, x, t, y, c):
+        """
+        Forward pass of DiT.
+        x: (N, C, P) tensor of spatial inputs (point clouds or latent representations of images)
+        t: (N,) tensor of diffusion timesteps
+        y: (N,) tensor of class labels
+        """
+        x = self.x_embedder(x) 
+
+        t = self.t_embedder(t).unsqueeze(1)
+        y = self.y_embedder(y, self.training)    
+        c = self.c_embedder(c)
+        c = torch.cat((t, y, c), dim=1).flatten(1)
+
+        x = x.transpose(-1,-2)
+        for block in self.blocks:
+            x = block(x, c)                      
+        
+        x = self.final_layer(x, c)    
+
+        return x.transpose(-1,-2)
+
+    def forward_with_cfg(self, x, t, y, cfg_scale):
+        """
+        Forward pass of DiT, but also batches the unconditional forward pass for classifier-free guidance.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/notebooks/text2im.ipynb
+        half = x[: len(x) // 2]
+        combined = torch.cat([half, half], dim=0)
+        model_out = self.forward(combined, t, y)
+        # For exact reproducibility reasons, we apply classifier-free guidance on only
+        # three channels by default. The standard approach to cfg applies it to all channels.
+        # This can be done by uncommenting the following line and commenting-out the line following that.
+        # eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
+        eps, rest = model_out[:, :3], model_out[:, 3:]
+        cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
+        half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
+        eps = torch.cat([half_eps, half_eps], dim=0)
+        return torch.cat([eps, rest], dim=1)
+
+
+#################################################################################
+#                                   DiT Configs                                  #
+#################################################################################
+def DiT_XL_2(pretrained=False, **kwargs):
+
+    model = DiT(depth=28, hidden_size=1152, patch_size=2, num_heads=16, **kwargs)
+    if pretrained:
+        checkpoint = torch.load('/home/ekirby/workspace/DiT-3D/checkpoints/DiT-XL-2-512x512.pt', map_location='cpu')
+        if "ema" in checkpoint:  # supports ema checkpoints 
+            checkpoint = checkpoint["ema"]
+        checkpoint_blocks = {k: checkpoint[k] for k in checkpoint if k.startswith('blocks')}
+        # load pre-trained blocks from 2d DiT
+        msg = model.load_state_dict(checkpoint_blocks, strict=False)
+
+    return model
+
+def DiT_XL_4(pretrained=False, **kwargs):
+
+    model = DiT(depth=28, hidden_size=1152, patch_size=4, num_heads=16, **kwargs)
+    if pretrained:
+        checkpoint = torch.load('/path/to/DiT2D_pretrained_weights/DiT-XL-2-512x512.pt', map_location='cpu')
+        if "ema" in checkpoint:  # supports ema checkpoints 
+            checkpoint = checkpoint["ema"]
+        checkpoint_blocks = {k: checkpoint[k] for k in checkpoint if k.startswith('blocks')}
+        # load pre-trained blocks from 2d DiT
+        msg = model.load_state_dict(checkpoint_blocks, strict=False)
+    
+    return model
+
+def DiT_XL_8(pretrained=False, **kwargs):
+
+    model = DiT(depth=28, hidden_size=1152, patch_size=8, num_heads=16, **kwargs)
+    if pretrained:
+        checkpoint = torch.load('/path/to/DiT2D_pretrained_weights/DiT-XL-2-512x512.pt', map_location='cpu')
+        if "ema" in checkpoint:  # supports ema checkpoints 
+            checkpoint = checkpoint["ema"]
+        checkpoint_blocks = {k: checkpoint[k] for k in checkpoint if not k.startswith('blocks')}
+        # load pre-trained blocks from 2d DiT
+        msg = model.load_state_dict(checkpoint_blocks, strict=False)
+
+    return model
+
+def DiT_L_2(pretrained=False, **kwargs):
+    return DiT(depth=24, hidden_size=1152, num_heads=16, **kwargs)
+
+def DiT_L_4(pretrained=False, **kwargs):
+    return DiT(depth=24, hidden_size=1152, num_heads=16, **kwargs)
+
+def DiT_L_8(pretrained=False, **kwargs):
+    return DiT(depth=24, hidden_size=1152, num_heads=16, **kwargs)
+
+def DiT_B_2(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=768, num_heads=12, **kwargs)
+
+def DiT_B_4(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=768, num_heads=12, **kwargs)
+
+def DiT_B_8(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=768, num_heads=12, **kwargs)
+
+def DiT_B_16(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=768, num_heads=12, **kwargs)
+
+def DiT_B_32(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=768, num_heads=12, **kwargs)
+
+def DiT_S_2(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=384, num_heads=6, **kwargs)
+
+def DiT_S_4(pretrained=False, **kwargs):
+
+    model = DiT(depth=12, hidden_size=192, num_heads=3, **kwargs)
+    if pretrained:
+        checkpoint = torch.load('/home/ekirby/workspace/DiT-3D/checkpoints/shapenet_s_4_ema_1/shapenet_s_4_ema_1_epoch=9999.ckpt', map_location='cpu')
+        if "ema" in checkpoint:  # supports ema checkpoints 
+            checkpoint = checkpoint["ema"]
+        checkpoint_blocks = {k: checkpoint[k] for k in checkpoint if k.startswith('blocks')}
+        # load pre-trained blocks from 2d DiT
+        msg = model.load_state_dict(checkpoint_blocks, strict=False)
+
+    return model
+
+def DiT_S_8(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=384, num_heads=6, **kwargs)
+
+def DiT_S_16(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=384, num_heads=6, **kwargs)
+
+def DiT_S_32(pretrained=False, **kwargs):
+    return DiT(depth=12, hidden_size=384, num_heads=6, **kwargs)
+
+DiT3D_models_FlashAttn = {
+    'DiT-XL/2': DiT_XL_2,  'DiT-XL/4': DiT_XL_4,  'DiT-XL/8': DiT_XL_8,
+    'DiT-L/2':  DiT_L_2,   'DiT-L/4':  DiT_L_4,   'DiT-L/8':  DiT_L_8,
+    'DiT-B/2':  DiT_B_2,   'DiT-B/4':  DiT_B_4,   'DiT-B/8':  DiT_B_8,
+    'DiT-S/2':  DiT_S_2,   'DiT-S/4':  DiT_S_4,   'DiT-S/8':  DiT_S_8,
+}
