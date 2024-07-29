6673e860625c4bdef86d7d70c09b6017205ab15b
Ellington Kirby <ellingtonkirby31@gmail.com>
2024-07-29 09:33:20 +0000

    move configs

--- scripts/eval_dits_2.sh
+++ scripts/eval_dits_2.sh
@@ -5,5 +5,5 @@ pip install .
 
 cd /home/ekirby/workspace/DiT-3D
 
-python train_dit3d.py -c config/ablation_3/xs_4_1a_pixart_modadaln_pnet_impcfg_4ch.yaml -w checkpoints/xs_4_1a_pixart_pointnet_modadaln_impcfg_4ch_2/last.ckpt --test
-python train_dit3d.py -c config/ablation_3/xs_4_1a_just_cross_4ch.yaml  -w checkpoints/xs_4_1a_just_cross_4ch/last.ckpt --test
+python train_dit3d.py -c config/ablation_4/xs_4_1a_cross_pointnet_noff.yaml -w checkpoints/xs_4_1a_cross_pointnet_impcgf_4ch_noff/last.ckpt --test
+python train_dit3d.py -c config/ablation_4/xs_4_1a_cross_pointnet_reordered.yaml  -w checkpoints/xs_4_1a_cross_pointnet_impcgf_4ch_reordered/last.ckpt --test
--- scripts/run_object_gen.sh
+++ scripts/run_object_gen.sh
@@ -1,17 +1,17 @@
 # !/bin/bash
-# cd /home/ekirby/workspace/DiT-3D/metrics/PyTorchEMD
-# pip install .
+cd /home/ekirby/workspace/DiT-3D/metrics/PyTorchEMD
+pip install .
 
 cd /home/ekirby/workspace/DiT-3D
 
-config=config/ablation_3/xs_4_1a_pixart_modadaln_pnet_impcfg_4ch.yaml
-weights=checkpoints/xs_4_1a_pixart_pointnet_modadaln_impcfg_4ch_2/last.ckpt
+config=config/ablation_4/xs_4_1a_cross_pointnet_noff.yaml
+weights=checkpoints/xs_4_1a_cross_pointnet_impcgf_4ch_noff/last.ckpt
 # python object_gen_eval_impcfg.py -c $config -w $weights -n xs_4_1a_cross_pnet_impcfg_4ch_eval_14_f -e 1 -t recreate -cls None -ds 1 -ind 14
-python object_gen_eval_impcfg.py -c $config -w $weights -n xs_4_1a_pixart_pnet_2_impcfg_4ch_eval_14_a -e 1 -t interpolate -cls None -ind 14
+python object_gen_eval_impcfg.py -c $config -w $weights -n xs_4_1a_cross_pointnet_noff_4ch_eval_14_a -e 1 -t interpolate -cls None -ind 14
 
-# config=config/ablation_3/xs_4_1a_just_cross_4ch.yaml
-# weights=checkpoints/xs_4_1a_just_cross_4ch/last.ckpt
-# python object_gen_eval_impcfg.py -c $config -w $weights -n xs_4_1a_just_cross_eval_14_a -e 1 -t interpolate -cls None -ind 14
+config=config/ablation_4/xs_4_1a_cross_pointnet_reordered.yaml
+weights=checkpoints/xs_4_1a_cross_pointnet_impcgf_4ch_reordered/last.ckpt
+python object_gen_eval_impcfg.py -c $config -w $weights -n xs_4_1a_cross_pointnet_reordered_eval_14_a -e 1 -t interpolate -cls None -ind 14
 
 # config=config/ablation_3/xs_4_1a_pixart_pnet_impcfg.yaml
 # weights=checkpoints/xs_4_1a_pixart_pointnet_dimsinadaln_impcfg/last.ckpt
